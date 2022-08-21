e5fa575845423eaaa236f74b228ccba0303dc56c
SHUN5872698 <tekisasu.bager@gmail.com>
2022-08-21 16:31:35 +0900

    Postコントローラー用のルート設定を追加

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
 Auth::routes();
@@ -24,3 +24,5 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('/post', 'PostController');
