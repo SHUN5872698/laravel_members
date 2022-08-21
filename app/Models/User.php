b24e8d3a98d42b18a170168a7ec03a0f5e4eb9e0
SHUN5872698 <tekisasu.bager@gmail.com>
2022-08-21 10:48:47 +0900

    postモデルとのリレーション情報を記入

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+use App\Models\Post;
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -41,4 +43,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * postモデルとのリレーション
+     *
+     * @return void
+     */
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
