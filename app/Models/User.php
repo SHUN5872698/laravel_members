9b9686ff12fcd0b6646318e48f1edca627d37664
SHUN5872698 <tekisasu.bager@gmail.com>
2022-08-21 14:23:26 +0900

    インデントサイズを強制的に2に変更

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,45 +12,45 @@ use App\Models\Post;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+  use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'name',
+    'email',
+    'password',
+  ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var array<int, string>
+   */
+  protected $hidden = [
+    'password',
+    'remember_token',
+  ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array<string, string>
+   */
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+  ];
 
-    /**
-     * postモデルとのリレーション
-     *
-     * @return void
-     */
-    public function posts()
-    {
-        return $this->hasMany(Post::class);
-    }
+  /**
+   * postモデルとのリレーション
+   *
+   * @return void
+   */
+  public function posts()
+  {
+    return $this->hasMany(Post::class);
+  }
 }
