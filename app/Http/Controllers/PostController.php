276e7ee514a87404e5f2b7f340173e634af38c93
SHUN5872698 <tekisasu.bager@gmail.com>
2022-08-28 13:53:47 +0900

    インデントを揃える

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,93 +8,93 @@ use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function index()
-	{
-		//
-	}
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    //
+  }
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function create()
-	{
-		return view('post.create');
-	}
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    return view('post.create');
+  }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @return \Illuminate\Http\Response
-	 */
-	public function store(Request $request)
-	{
-		// バリデーションルール
-		$inputs = $request->validate([
-			'title' => 'required|max:255',
-			'body' => 'required|max:255',
-			'image' => 'image|max:1024'
-		]);
-		// インスタンスを作成
-		$post = new Post();
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    // バリデーションルール
+    $inputs = $request->validate([
+      'title' => 'required|max:255',
+      'body' => 'required|max:255',
+      'image' => 'image|max:1024'
+    ]);
+    // インスタンスを作成
+    $post = new Post();
 
-		$post->title = $request->title;
-		$post->body = $request->body;
-		$post->user_id = auth()->user()->id;
-		// 保存
-		$post->save();
-		return back()->with('message', '投稿を作成しました');
-	}
+    $post->title = $request->title;
+    $post->body = $request->body;
+    $post->user_id = auth()->user()->id;
+    // 保存
+    $post->save();
+    return back()->with('message', '投稿を作成しました');
+  }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function show($id)
-	{
-		//
-	}
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    //
+  }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function edit($id)
+  {
+    //
+  }
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  int  $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function update(Request $request, $id)
-	{
-		//
-	}
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    //
+  }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    //
+  }
 }
