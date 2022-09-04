d583806e36382db311cd400858498a3011f534db
SHUN5872698 <tekisasu.bager@gmail.com>
2022-09-04 17:02:35 +0900

    重複ルート処理を削除

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-	return view('welcome');
+  return view('welcome');
 });
 
 Auth::routes();
@@ -21,5 +21,3 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('/post', 'PostController');
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
