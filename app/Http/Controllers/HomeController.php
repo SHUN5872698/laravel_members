53d23160cc02ce3b21d0e9ae5144662879d13ff5
SHUN5872698 <tekisasu.bager@gmail.com>
2022-09-04 15:53:15 +0900

    投稿一覧ページの処理を作成

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,26 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->middleware('auth');
+  }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-        return view('home');
-    }
+  /**
+   * Show the application dashboard.
+   *
+   * @return \Illuminate\Contracts\Support\Renderable
+   */
+  // public function index()
+  // {
+  //     return view('home');
+  // }
+
+  /**
+   * 投稿一覧ページ
+   *
+   * @return void
+   */
+  public function index()
+  {
+    // 投稿内容作成順で全て取得
+    $posts = Post::orderBy('created_at', 'desc')->get();
+    // ログインユーザー
+    $user = auth()->user();
+    return view('home', compact('posts', 'user'));
+  }
 }
