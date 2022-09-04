f007fccd37f509ea06b368e4eb807eab2e21862f
SHUN5872698 <tekisasu.bager@gmail.com>
2022-09-04 15:55:44 +0900

    投稿一覧ページ作成

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -20,4 +20,33 @@
         </div>
     </div>
 </div>
+@endsection --}}
+
+@extends('layouts.app')
+@section('content')
+    <div class="container-fluid mt-20" style="margin-left:-10px;">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <p class="my-3">{{ $user->name }}さん、こんにちは！</p>
+                @foreach ($posts as $post)
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            <div class="media flex-wrap w-100 align-items-center">
+                                <div class="media-body ml-3"> {{ $post->title }}
+                                    <div class="text-muted small"> {{ $post->user->name }}</div>
+                                </div>
+                                <div class="text-muted small ml-3">
+                                    <div>投稿日</div>
+                                    <div><strong>{{ $post->created_at->diffForHumans() }}</strong></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <p>{{ $post->body }} </p>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
 @endsection
